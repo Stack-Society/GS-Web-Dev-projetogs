e49683b1143644b6297a1ade1f9aa4bc96af757b
giovanni <giovannirprovazi@gmail.com>
2025-06-06 20:23:20 -0300

    Adicionando logo ao projeto

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Logo from '../assets/ilustracao-logo.jpg'
+import Logo from '../assets/logo.png'
 
 const Nav = () => {
   return (
